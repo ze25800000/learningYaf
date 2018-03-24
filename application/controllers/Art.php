d68b368e86cdfb2c3fca0afa43ce893ace35f2db
ze25800000 <ze258100000@sina.com>
2018-03-24 15:40:49 +0800

    文章类API-新增和修改

--- application/controllers/Art.php
+++ application/controllers/Art.php
@@ -12,16 +12,76 @@ class ArtController extends Yaf_Controller_Abstract {
 	}
 
 	public function addAction( $artId = 0 ) {
-		if ( $this->_isAdmin() ) {
+		if ( ! $this->_isAdmin() ) {
 			echo json_encode( [
 				'errno'  => - 2000,
 				"errmsg" => "需要管理员才可以操作"
 			] );
+
+			return false;
 		}
+		$submit = $this->getRequest()->getQuery( 'submit', 0 );
+		if ( $submit != '1' ) {
+			echo json_encode( [
+				'errno'  => - 2001,
+				'errmsg' => '请通过正确渠道提交'
+			] );
+
+			return false;
+		}
+		$title    = $this->getRequest()->getPost( 'title', false );
+		$contents = $this->getRequest()->getPost( 'contents', false );
+		$author   = $this->getRequest()->getPost( 'author', false );
+		$cate     = $this->getRequest()->getPost( 'cate', false );
+
+		if ( ! $title || ! $contents || ! $author || ! $cate ) {
+			echo json_encode( [
+				'errno'  => - 2002,
+				'errmsg' => '标题、内容、作者、分类信息，不能为空'
+			] );
+
+			return false;
+		}
+		// 调用Model ,做登录验证
+		$model = new ArtModel();
+		if ( $lastId = $model->add( trim( $title ), trim( $contents ), trim( $author ), trim( $cate ), $artId ) ) {
+			echo json_encode( [
+				'errno'  => 0,
+				'errmsg' => '',
+				'data'   => [ 'lastId' => $lastId ]
+			] );
+		} else {
+			echo json_encode( [
+				'errno'  => $model->errno,
+				'errmsg' => $model->errmsg,
+			] );
+		}
+
+		return true;
 	}
 
 	public function editAction() {
+		if ( !$this->_isAdmin() ) {
+			echo json_encode( [
+				'errno'  => - 2000,
+				"errmsg" => "需要管理员才可以操作"
+			] );
 
+			return false;
+		}
+		$artId = $this->getRequest()->getQuery( 'artId', '0' );
+		if ( is_numeric( $artId ) && $artId ) {
+			$this->addAction( $artId );
+		} else {
+			echo json_encode( [
+				'errno'  => - 2003,
+				"errmsg" => "缺少必要的文章ID参数"
+			] );
+
+			return false;
+		}
+
+		return true;
 	}
 
 	public function delAction() {
@@ -41,6 +101,6 @@ class ArtController extends Yaf_Controller_Abstract {
 	}
 
 	private function _isAdmin() {
-
+		return true;
 	}
 }
--- application/models/Art.php
+++ application/models/Art.php
@@ -7,6 +7,62 @@ class ArtModel {
 
 	public function __construct() {
 		$this->_db = new PDO( 'mysql:host=127.0.0.1;dbname=yaf;', 'root', 'Yangze@1234' );
+		/**
+		 * 不设置下面这行的话，PDO会在拼SQL的时候，把int 0转成 string 0
+		 */
+		$this->_db->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
 	}
 
+	public function add( $title, $contents, $author, $cate, $artId = 0 ) {
+		$isEdit = false;
+		if ( $artId != 0 && is_numeric( $artId ) ) {
+			/* edit */
+			$query = $this->_db->prepare( "select count(*) from `art` WHERE `id`=?" );
+			$query->execute( [ $artId ] );
+			$ret = $query->fetchAll();
+			if ( ! $ret || count( $ret ) != 1 ) {
+				$this->errno  = - 2003;
+				$this->errmsg = "找不到你要的编辑文章";
+
+				return false;
+			}
+			$isEdit = true;
+		} else {
+			/* add */
+			$query = $this->_db->prepare( "select count(*) from `cate` WHERE `id`=?" );
+			$query->execute( [ $cate ] );
+			$ret = $query->fetchAll();
+			if ( ! $ret || $ret [0][0] == 0 ) {
+				$this->errno  = - 2005;
+				$this->errmsg = "找不到你要的分类信息，cate id：" . $cate . "，请先创建该分类";
+
+				return false;
+			}
+		}
+		/**
+		 * 插入或者更新文章
+		 */
+		$data = [ $title, $contents, $author, intval( $cate ) ];
+		if ( ! $isEdit ) {
+			$query = $this->_db->prepare( 'insert into `art` (`title`,`content`,`author`,`cate`) VALUES (?,?,?,?)' );
+		} else {
+			$query  = $this->_db->prepare( "UPDATE `art` SET title=?,content=?,author=?,cate=? WHERE id=?" );
+			$data[] = $artId;
+		}
+		$ret = $query->execute( $data );
+		if ( ! $ret ) {
+			$this->errno  = - 2006;
+			$this->errmsg = '操作文章数据表失败，ErrInfo:' . end( $query->errorInfo() );
+
+			return false;
+		}
+		/**
+		 * 返回文章最后的ID值
+		 */
+		if ( ! $isEdit ) {
+			return intval( $this->_db->lastInsertId() );
+		} else {
+			return intval( $artId );
+		}
+	}
 }
